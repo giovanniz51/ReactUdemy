b274b039196eb65b15cd27a379ed9f76bb6645db
Giovanni Zito <giovanniz51@Giovannis-MacBook-Pro.local>
2018-05-01 20:18:54 +0200

    Added Search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,14 @@
-import React, {Component} from "react";
+import React from "react";
 
 import classes from "../css/styles.css";
 
-export class Header extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            title: "The words are: ",
-            words: ""
-        }
-    }
+export const Header = (props) => {
 
-    inputChangeHandler = (event) => { //mit arrow function muss man nicht bind(this)
-        this.setState({
-            words: event.target.value
-        });
-    }
-
-    render(){
-
-        console.log(this.state.words);
         return (
             <header>
                 <div className={classes.logo}>Logo</div>
-                <input type="text" onChange={this.inputChangeHandler}/>
+                <input type="text" onChange={props.words}/>
             </header>
         )
-    }
     
 }
--- src/index.js
+++ src/index.js
@@ -11,15 +11,32 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            news: JSON
+            news: JSON,
+            filtered: []
         }
     }
 
+    getWord = (event) => {
+        //console.log(event.target.value);
+        let word = event.target.value;
+        let filtered = this.state.news.filter( (item) => {
+            return item.title.indexOf(word) > -1;
+        });
+
+        this.setState({
+            filtered
+        })
+
+        //console.log(filtered);
+    }
+
     render(){
+        let newsFiltered = this.state.filtered;
+        let newsWhole = this.state.news;
         return (
             <div>
-               <Header />
-               <NewsList news={this.state.news}>
+               <Header words={this.getWord}/>
+               <NewsList news={newsFiltered.length === 0 ? newsWhole : newsFiltered }>
                 <h3>The news are:</h3>
                </NewsList>
             </div>
